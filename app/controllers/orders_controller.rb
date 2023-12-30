c39eef6e406cbfee76b4f072f26782e040846e31
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2023-12-30 17:15:25 +0900

    payjpの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,11 @@ class OrdersController < ApplicationController
   before_action :find_item
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipping = OrderShipping.new
+  end
+
+  def create
   end
 
   private
