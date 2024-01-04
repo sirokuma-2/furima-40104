ab486f80f213c106583548d3c79ee82935af84f8
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-01-05 04:36:36 +0900

    アソシエーション　単体テストコード再度修正

--- spec/factories/order_shipping.rb
+++ spec/factories/order_shipping.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_shipping do
-    postal_code { '123-4567' }
-    prefecture_id { 1 }
-    city { 'テスト市' }
-    address { 'テスト1-1-1' }
-    building_name { 'テストビル101' }
-    phone_number { '09012345678' }
-    token { 'tok_abcdefghijk00000000000000000' }
+    postal_code { "#{Faker::Number.between(from: 100, to: 999)}-#{Faker::Number.between(from: 1000, to: 9999)}" }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    city { Faker::Address.city }
+    address { Faker::Address.street_address }
+    building_name { Faker::Address.building_number }
+    phone_number { "0#{Faker::Number.between(from: 100_000_000, to: 9_999_999_999)}" }
+    token { 'tok_e4942a5e6aa2bfb3b37463dfad46' }
   end
 end
