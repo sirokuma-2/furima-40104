75f618fa1e6aed1789e051d45eff5cc8385e74ff
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-01-07 17:05:30 +0900

    商品詳細表示修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    redirect_to root_path if Order.exists?(item_id: @item.id)
+    # redirect_to root_path if Order.exists?(item_id: @item.id)
   end
 
   def edit
